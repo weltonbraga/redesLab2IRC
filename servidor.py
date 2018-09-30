612ce2f6c7cf9bb43246489f21b76ff692855986
weltonbraga <webra>
2018-09-30 14:00:31 +0000

    implementado suporte a multi clientes

--- servidor.py
+++ servidor.py
@@ -1,44 +1,106 @@
 # https://pymotw.com/3/socket/tcp.html
-import socket
-import sys
+import socket, select, sys
+
+def send_to_all (sock, msg):
+    print('manda...')
+    message = msg.encode()
+    #Message not forwarded to server and sender itself
+    for socket in connected_list:
+        if socket != server_socket and socket != sock :
+            try :
+                socket.sendall(message)
+            except :
+                # if connection not available
+                socket.close()
+                connected_list.remove(socket)
+
 
 print('SERVIDOR')
-
-# lista de usuários online
-usuarios_online = []
+buffer = 1024
+#dictionary to store address corresponding to username
+record={}
+# List to keep track of socket descriptors
+connected_list = []
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
 server_address = ('localhost', 10001)
 print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+server_socket.bind(server_address)
 
 # Listen for incoming connections
-sock.listen(10)
+server_socket.listen(10)
+
+# Add server socket to the list of readable connections
+connected_list.append(server_socket)
 
 while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
+    # Get the list sockets which are ready to be read through select
+    rList,wList,error_sockets = select.select(connected_list,[],[], 0)
+    #print('l:', len(rList))
+    for sock in rList:
+        #print('sock:', sock)
+        #New connection
+        if sock is server_socket:
+            print('server')
+            # Handle the case in which there is a new connection recieved through server_socket
+            sockfd, addr = server_socket.accept()
+            name = sockfd.recv(buffer)
+            name = name.decode()
+            connected_list.append(sockfd)
+            record[addr]=""
+            #print ("record and conn list ",record,connected_list)
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print('received {!r}'.format(data))
-            data = data.decode()
-            data = data.upper()
-            data = data.encode()
-            if data:
-                print('sending data back to the client')
-                connection.sendall(data)
+            #if repeated username
+            if name in record.values():
+                mensagem = "\r\33[31m\33[1m Username already taken!\n\33[0m"
+                mensagem = mensagem.encode()
+                sockfd.send(mensagem)
+                del record[addr]
+                connected_list.remove(sockfd)
+                sockfd.close()
+                continue
             else:
-                print('no data from', client_address)
-                break
+                #add name and address
+                record[addr] = name
+                print ("Client {} connected [{}]".format(addr, record[addr]))
+                mensagem = "\33[32m\r\33[1m Welcome to chat room.\n\33[0m"
+                mensagem = mensagem.encode()
+                sockfd.sendall(mensagem)
+                send_to_all(sockfd, "\33[32m\33[1m\r "+ name + " joined the conversation \n\33[0m")
+                print('n:',sockfd.getpeername())
 
-    finally:
-        # Clean up the connection
-        connection.close()
+        #Some incoming message from a client
+        else:
+            # Data from client
+            print('Data from client')
+            try:
+                data = sock.recv(buffer)
+
+                ip, porta = sock.getpeername()
+                if not data:
+                    send_to_all(sock, "\r\33[31m \33[1m"+record[(ip,porta)]+" left the conversation unexpectedly\33[0m\n")
+                    del record[(ip,porta)]
+                    connected_list.remove(sock)
+                    sock.close()
+                else:
+                    data = data.decode()
+                    msg = "\r\33[1m"+"\33[35m "+record[(ip,porta)]+": "+"\33[0m"+data+"\n"
+                    send_to_all(sock, msg)
+
+            #abrupt user exit
+            except:
+                print('aki')
+                (ip,porta)=sock.getpeername()
+                send_to_all(sock, "\r\33[31m \33[1m"+record[(ip,porta)]+" left the conversation unexpectedly\33[0m\n")
+                print ("Client {} is offline (error) [{}]".format((ip,porta),record[(ip,porta)]))
+                del record[(ip,porta)]
+                connected_list.remove(sock)
+                sock.close()
+                continue
+
+
+
+server_socket.close()
